2e26990ba8cfeb94026ece8be053948a0d627d12
Shravya Reddy Gennepally <sh602929@dal.ca>
2022-07-11 22:50:34 -0300

    booking ride by passenger files added

---
+++ vehiclesharing/src/main/java/com/example/vehiclesharing/dao/BookingDAO.java
@@ -0,0 +1,17 @@
+package com.example.vehiclesharing.dao;
+
+import com.example.vehiclesharing.model.Booking;
+
+import java.util.List;
+
+public interface BookingDAO {
+
+    public boolean saveRide(Booking booking);
+
+    //This method is used to insert booking object into the database
+
+    public List<Booking> getAllRidesForCustomer(int customer_id);
+
+    public boolean updateIsPaid(int customer_id, int booked_ride_id);
+
+}
